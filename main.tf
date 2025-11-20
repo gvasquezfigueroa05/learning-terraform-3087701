0c9ce1d5a7483e4e89a1b52f2391210d68773552
gvasquezfigueroa05 <gvasquezfigueroa@gmail.com>
2025-11-20 13:35:48 -0500

    alb 2

--- main.tf
+++ main.tf
@@ -55,28 +55,26 @@ module "alb" {
   subnets         =  module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  listeners = [
-    {
+listeners = {
+    ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
-      target_group_index = 0
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
-    ]
 
-  target_groups = [
-    {
+  target_groups = {
+    ex-instance = {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id        = aws_instance.blog.id
     }
-  ]
+  }
 
   tags = {
     Environment = "Development"
